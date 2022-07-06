e54497de7efb7c91125191f1f3525b96c156308c
Lucas Viana <oviana@outlook.com>
2022-07-06 14:11:29 -0300

    Add documentation

--- ex06/main.py
+++ ex06/main.py
@@ -9,35 +9,7 @@ from enum import Enum
 
 import imageio
 import numpy as np
-from matplotlib import pyplot as plt
 from numpy.typing import NDArray
-
-
-def plot(imgs: list[dict], n_rows: int, n_cols: int, axis=True) -> None:
-    """
-    Plot a list of images using MatPlotLib.
-
-    Parameters
-    ----------
-    imgs ({ "title": str, "images": NDArray }): images to be plotted
-    n_rows (int): number of rows in the grid
-    n_cols (int): number of columns in the grid
-    axis (boolean): should the axis be shown in the subplots
-    """
-    actual_n_rows = len(imgs) // n_cols
-    n_cols_last_row = len(imgs) - (actual_n_rows - 1) * n_cols
-
-    for i in range(1, actual_n_rows + 1):
-        for j in range(1, n_cols_last_row + 1):
-            img = imgs[(index := (i - 1) * n_cols + j) - 1]
-            plt.subplot(int(f"{n_rows}{n_cols}{index}"))
-            plt.axis("on" if axis else "off")
-            plt.title(img["title"])
-            plt.imshow(
-                img["image"].astype(np.uint8),
-                cmap=img["cmap"] if "cmap" in img else "gray",
-            )
-    plt.show()
 
 
 class PixelAttributeOption(Enum):
@@ -63,7 +35,7 @@ def norm(img: NDArray, new_max=255) -> NDArray:
     The normalized image
     """
     max_value, min_value = img.max(), img.min()
-    return ((img - min_value) / (max_value - min_value)) * new_max
+    return (((img - min_value) / (max_value - min_value)) * new_max).astype(np.float32)
 
 
 def multi_channel_norm(img: NDArray, new_max=255) -> NDArray:
@@ -80,12 +52,12 @@ def multi_channel_norm(img: NDArray, new_max=255) -> NDArray:
     The normalized image
     """
     normalized = img.copy()
-    for i in np.arange(img.shape[2]):
+    for i in range(img.shape[2]):
         img[:, :, i] = norm(img[:, :, i], new_max)
     return normalized
 
 
-def rmse(image1: NDArray[np.uint8], image2: NDArray[np.uint8]) -> float:
+def rmse(image1: NDArray[np.float32], image2: NDArray[np.float32]) -> float:
     """
     Calculate the Root Mean Square Error (RMSE) between two single channel images (e.g.: grayscale).
 
@@ -99,7 +71,7 @@ def rmse(image1: NDArray[np.uint8], image2: NDArray[np.uint8]) -> float:
     return np.sqrt(np.sum((image1 - image2) ** 2) / (m * n))
 
 
-def multi_channel_rmse(image1: NDArray[np.uint8], image2: NDArray[np.uint8]) -> float:
+def multi_channel_rmse(image1: NDArray, image2: NDArray) -> float:
     """
     Calculate the Root Mean Square Error (RMSE) between two multi channel images (e.g.: RGB).
 
@@ -110,7 +82,7 @@ def multi_channel_rmse(image1: NDArray[np.uint8], image2: NDArray[np.uint8]) -> 
     float: the error between the two matrices
     """
     acc = 0
-    for i in np.arange(n_channels := image1.shape[2]):
+    for i in range(n_channels := image1.shape[2]):
         acc += rmse(image1[:, :, i], image2[:, :, i])
     return acc / n_channels
 
@@ -132,23 +104,37 @@ def grayscale(img: NDArray) -> NDArray[np.number]:
 
 
 def reshape(matrix: NDArray) -> NDArray:
+    """Reshape a "matrix" in which each element may be a numpy array to a "row" array, maintaining each element as it is."""
     n_channels = matrix.shape[2] if len(matrix.shape) == 3 else 1
     m, n = matrix.shape[:2]
     return matrix.reshape((m * n, n_channels))
 
 
-def coordinates_matrix(m, n):
+def coordinates_matrix(m: int, n: int) -> NDArray:
+    """
+    Generate a matrix in which each element is an array containing that element's row and column index.
+
+    Parameters
+    ----------
+    m: number of rows in the matrix
+    n: number of columns in the matrix
+
+    Returns
+    -------
+    The matrix containing the coordinates
+    """
     x_coords = np.tile(np.arange(m).reshape((m, 1)), (1, n)).reshape((m * n, 1))
     y_coords = np.tile(np.arange(n).reshape((1, n)), (m, 1)).reshape((m * n, 1))
     return np.concatenate((x_coords, y_coords), axis=1)
 
 
-def distance(p: NDArray[np.float32], q: NDArray[np.float32]) -> float:
+def euclidian_dist(p: NDArray[np.float32], q: NDArray[np.float32], axis=1) -> NDArray:
     """Calculate the euclidian distance between two given R^N vectors of same shape."""
-    return ((p - q) ** 2).sum() / p.shape[0]
+    return ((p - q) ** 2).sum(axis=axis) / p.shape[0]
 
 
 def parse_dataset_from_img(img: NDArray, option: PixelAttributeOption) -> NDArray:
+    """Convert an image to a dataset with the chosen attributes."""
     evaluate = {
         PixelAttributeOption.RGB: lambda img: reshape(img),
         PixelAttributeOption.RGBxy: lambda img: np.concatenate(
@@ -163,9 +149,8 @@ def parse_dataset_from_img(img: NDArray, option: PixelAttributeOption) -> NDArra
     return evaluate(img)
 
 
-def parse_img_from_features(
-    features: NDArray, option: PixelAttributeOption
-) -> NDArray:
+def parse_img_from_features(features: NDArray, option: PixelAttributeOption) -> NDArray:
+    """Convert a matrix of features (according to the chosen attributes) into an RGB or grayscale image."""
     evaluate = {
         PixelAttributeOption.RGB: lambda R, G, B: [R, G, B],
         PixelAttributeOption.RGBxy: lambda R, G, B, x, y: [R, G, B],
@@ -183,40 +168,73 @@ def parse_img_from_features(
     m, n = features.shape[:2]
     img = np.zeros((m, n, 3) if multi_channel else (m, n))
 
-    for i in np.arange(m):
-        for j in np.arange(n):
-            img[i, j] = evaluate(*features[i, j]) if multi_channel else evaluate(features[i, j])
+    for i in range(m):
+        for j in range(n):
+            img[i, j] = (
+                evaluate(*features[i, j])
+                if option != PixelAttributeOption.Luminance
+                else evaluate(features[i, j])
+            )
 
     return img.astype(np.float32)
 
 
-def k_means(k: int, img: NDArray, n_iterations: int):
+def k_means(k: int, img: NDArray, n_iterations: int, seed: int) -> NDArray:
+    """
+    Partition an image to a given number of cluster.
+
+    Parameters
+    ----------
+    k: number of clusters
+    img: target image
+    n_iterations: maximum number of iterations to be executed
+    seed: seed to use in random generations
+
+    Returns
+    -------
+    The clusterized image
+    """
     m, n = img.shape[:2]
-    dataset = parse_dataset_from_img(img, attributes)
-    clusters = np.zeros(m * n).astype(np.uint32)
+    clusters = np.zeros(m * n).astype(np.float32)
+    features = parse_dataset_from_img(img, attributes)
+    _, n_attributes = features.shape
 
     # initialize centroids
-    initial_centroids_indexes = np.sort(random.sample(range(m * n), k))
-    centroids = np.array([dataset[i] for i in initial_centroids_indexes])
+    random.seed(seed)
+    centroids = features[random.sample(range(m * n), k)]
 
-    for _ in np.arange(n_iterations):
+    for _ in range(n_iterations):
+        # calculate distances between each pixel and each centroid
+        distances = np.empty((m * n, 0))
+        for centroid in centroids:
+            centroid_vec = np.full((m * n, n_attributes), centroid)
+            curr_distances = euclidian_dist(features, centroid_vec).reshape((m * n, 1))
+            distances = np.append(distances, curr_distances, axis=1)
+
         # assign each pixel to closest centroid
-        for i in np.arange(m * n):
-            distances = [distance(centroid, dataset[i]) for centroid in centroids]
-            clusters[i] = np.argmin(distances)
+        clusters = distances.argmin(axis=1)
 
         # recalculate centroids
-        for i in np.arange(k):
-            centroids[i] = (
-                np.mean(dataset[slice])
-                if (slice := clusters == i).any()
-                else centroids[i]
-            )
+        prev_centroids = centroids.copy()
+        for i in range(k):
+            centroids[i] = np.mean(features[clusters == i], axis=0)
 
-    return centroids[clusters].reshape((m, n))
+        # check convergence
+        if (prev_centroids == centroids).all():
+            break
+
+    # apply the clusters values to each pixel
+    # and reshape to match the image's shape
+    output = centroids[clusters].reshape(
+        (*img.shape[:2], centroids.shape[1])
+        if len(centroids.shape) == 2
+        else img.shape[:2]
+    )
+    return parse_img_from_features(output, attributes)
 
 
 if __name__ == "__main__":
+    # read input
     input_img_filename = input().strip()
     ref_img_filename = input().strip()
     attributes = PixelAttributeOption(int(input().strip()))
@@ -224,28 +242,21 @@ if __name__ == "__main__":
     n_iterations = int(input().strip())
     seed = int(input().strip())
 
-    random.seed(seed)
-
+    # setup
     input_img = imageio.imread(input_img_filename).astype(np.float32)
-    ref_img = imageio.imread(ref_img_filename)
+    ref_img = imageio.imread(ref_img_filename).astype(np.float32)
     multi_channel = len(ref_img.shape) == 3
 
-    clusters = k_means(n_clusters, input_img, n_iterations)
-    output_img = parse_img_from_features(clusters, attributes)
-    output_img = multi_channel_norm(output_img) if multi_channel else norm(output_img)
+    # apply k-means and normalize the output
+    clusterized_img = k_means(n_clusters, input_img, n_iterations, seed)
+    output_img = (
+        multi_channel_norm(clusterized_img) if multi_channel else norm(clusterized_img)
+    )
 
-    print(
+    # output the RMSE
+    root_squared_error = (
         multi_channel_rmse(ref_img, output_img)
         if multi_channel
         else rmse(ref_img, output_img)
     )
-
-    plot(
-        [
-            {"title": "Original", "image": input_img, "cmap": "viridis"},
-            {"title": "Theirs", "image": ref_img, "cmap": "viridis"},
-            {"title": "Mine", "image": output_img, "cmap": "viridis"},
-        ],
-        1,
-        3,
-    )
+    print(round(root_squared_error, 4))
